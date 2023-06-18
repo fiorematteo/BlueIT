687c72a317848d6f1c2d1447c27f9d24668fb616
CattaOS <87370954+cattaos@users.noreply.github.com>
2023-06-18 16:11:41 +0200

    Debugged excel.py

--- Report_Settimanale/excel.py
+++ Report_Settimanale/excel.py
@@ -42,6 +42,7 @@ alignment = Alignment(
 	wrap_text=True
 	)
 
+
 async def get_date() -> str:
 	t = time.localtime(time.time())
 	date_time = datetime.datetime(t.tm_year, t.tm_mon, t.tm_mday, 9, 0)
@@ -92,7 +93,7 @@ async def wr_xlsx(offenses_list: list) -> None:
 		if show_value:
 			counters.append(key)
 	ws.append(counters)
-	for idx in range(len(counters) - 1):
+	for idx in range(len(counters)):
 		r = ws[f'{ascii_uppercase[idx]}4']
 		r.font = Font(
 			name=font_name,
@@ -107,7 +108,7 @@ async def wr_xlsx(offenses_list: list) -> None:
 	for key in counters:
 		values.append(offenses_list[0][key])
 	ws.append(values)
-	for idx in range(len(counters) - 1):
+	for idx in range(len(counters)):
 		r = ws[f'{ascii_uppercase[idx]}5']
 		r.font = Font(
 			name=font_name,
@@ -121,9 +122,11 @@ async def wr_xlsx(offenses_list: list) -> None:
 
 	ws.merge_cells("A6:D24")
 
-	ws.append(["ID", "Data/Ora", "Minaccia", "Categoria"])
-	for idx in range(3):
+	ws.append([""])
+	mylist: list = ["ID", "Data/Ora", "Minaccia", "Categoria"]
+	for idx in range(4):
 		r = ws[f'{ascii_uppercase[idx]}25']
+		r.value = mylist[idx]
 		r.font = Font(
 			name=font_name,
 			size=10,
@@ -138,7 +141,7 @@ async def wr_xlsx(offenses_list: list) -> None:
 	row: int = 26
 	for offense in offenses_list:
 		ws.append([offense["id"], offense["start_time"], offense["description"]])
-		for idx in range(3):
+		for idx in range(4):
 			r = ws[f'{ascii_uppercase[idx]}{row}']
 			r.font = Font(
 				name=font_name,
@@ -150,63 +153,31 @@ async def wr_xlsx(offenses_list: list) -> None:
 			r.border = border
 			r.alignment = alignment
 		row += 1
+		ws.append([offense["note"]])
 		ws.merge_cells(f"A{row}:D{row}")
-		r = ws[f"A{row}"]
-		r.border = border
-		r.alignment = Alignment(
-			horizontal='left',
-			vertical='center',
-			wrap_text=True
-			)
+		for idx in range(4):
+			r = ws[f'{ascii_uppercase[idx]}{row}']
+			r.font = Font(
+				name=font_name,
+				size=7,
+				bold=False,
+				color='FF000000'
+				)
+			r.border = border
+			r.alignment = Alignment(
+				horizontal='left',
+				vertical='center',
+				wrap_text=True
+				)
+		row += 1
 
-	wb.save(f'C:\\Users\\andre\Downloads\TEST.xlsx')
+	wb.save(f'C:\\Download\TEST.xlsx')
 
 
 if __name__ == "__main__":
-	wb: Workbook = Workbook()
-	ws = wb.active
-	ws.append(["Resoconto settimanale S.O.C."])
-	r = ws["A1"]
-	r.font = Font(
-		name=font_name,
-		size=10,
-		bold=False,
-		color='FF0000'
-		)
-	r.fill = dark_fill
-	r.border = Border(
-		right=Side(
-			border_style="thick",
-			color=border_color
-			)
-		)
-	r.alignment = Alignment(
-		horizontal='center',
-		vertical='center',
-		wrap_text=True
-		)
-
-	ws.append([asyncio.run(get_date())])
-	r = ws["A2"]
-	r.font = Font(
-		name=font_name,
-		size=16,
-		bold=True
-		)
-	r.fill = light_fill
-	r.border = Border(
-		right=Side(
-			border_style="thick",
-			color=border_color
-			),
-		bottom=Side(
-			border_style="thick",
-			color=border_color
-			)
-		)
-	r.alignment = Alignment(
-		horizontal='center',
-		vertical='center',
-		wrap_text=True
-		)
-	wb.save(f'C:\\Users\\andre\Downloads\TEST.xlsx')
+	mylist: list = [
+		{"Allarmi totali": 50, "Breach alert di Darktrace": 30, "show_dt": True, "Allarmi di Sophos": 0, "show_sophos": False, "Offensive di QRadar": 20},
+		{"id": 69, "start_time": "06/12/2020", "description": "Desc Lorem ipsum", "note": "Note Lorem ipsum"},
+		{"id": 420, "start_time": "25/12/2020", "description": "Desc Lorem ipsum", "note": "Note Lorem ipsum"}
+		]
+	asyncio.run(wr_xlsx(mylist))
